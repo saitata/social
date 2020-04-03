46a6ad91867e3662970e729489738f83e573f38f
saitata <sravan19sai@gmail.com>
2020-04-03 18:47:11 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LoginPage from './login'
+ReactDOM.render(<LoginPage/>,document.getElementById('root'))
---
+++ src/login.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import axios from 'axios'
+import Dashboard from './post'
+import {BrowserRouter,Route,Redirect} from 'react-router-dom'
+
+
+
+class LoginPage extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            email:'',
+            status:false,
+            users:[],
+            emails:[],
+            LoginStatus:false,
+            redirect:null
+        }
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                const users=response.data
+                const emails=users.map(user=>user.email)
+                this.setState({users,emails})
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    handleEmail=(e)=>{
+     const email=e.target.value
+     this.setState({email})
+     localStorage.setItem('email',email)
+    }
+    handleChange=()=>{
+        if(!this.state.emails.includes(this.state.email)){
+            this.setState(prevState=>({
+                status:!prevState.status
+            }))
+        }
+        else{
+            this.setState({redirect:"/Dashboard",LoginStatus:true})
+        }
+    }
+    render(){
+        return(
+         <BrowserRouter>
+            <div>
+                {this.state.LoginStatus== false && (
+                    <div>
+                     <h2>LOGIN</h2>
+                    <input type="text" value={this.state.email} onChange={this.handleEmail} onBlur={this.handleChange} />
+                    {(this.state.status) && (<div><p>Invalid email / Syntax Error. Please type again</p></div>)} <br/><br/>
+                    </div>
+                )}
+                    {this.state.LoginStatus && (
+                     <div>
+                        <Redirect to={this.state.redirect} />
+                        <Route path="/Dashboard" component={Dashboard}  />
+                     </div>
+
+                    )}  
+            </div>
+         </BrowserRouter> 
+        )
+    }
+}   
+export default  LoginPage
